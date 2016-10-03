50b02d21070922d4ebfa5ee63e1ef304fd043d8b
Cameron Dykes <yellow5c@gmail.com>
2016-10-03 15:54:54 -0500

    Add KV.Bucket.delete/2

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -12,4 +12,13 @@ defmodule KV.BucketTest do
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
   end
+
+  test "removes values by key returning current value", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+    assert KV.Bucket.delete(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 4)
+    assert KV.Bucket.delete(bucket, "milk") == 4
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
 end
